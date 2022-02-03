31072474db183efa0d0f57eb9faafce6792ef23b
Adrian Wiśniewski <ardianows@gmail.com>
2022-02-03 03:11:03 +0100

    initial commit

---
+++ src/test/java/com/epam/ld/module2/testing/MailServerTest.java
@@ -0,0 +1,13 @@
+package com.epam.ld.module2.testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MailServerTest {
+    @Test
+    public void test() {
+
+    }
+
+}
