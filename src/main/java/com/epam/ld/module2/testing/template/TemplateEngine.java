ebf7cb98c4dfa30791aa48ae93f5fdb0501fd7a0
Adrian Wiśniewski <ardianows@gmail.com>
2022-02-04 04:25:24 +0100

    placeholderValueNotProvidedException

--- src/main/java/com/epam/ld/module2/testing/template/TemplateEngine.java
+++ src/main/java/com/epam/ld/module2/testing/template/TemplateEngine.java
@@ -2,6 +2,7 @@ package com.epam.ld.module2.testing.template;
 
 import com.epam.ld.module2.testing.Client;
 
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -23,8 +24,14 @@ public class TemplateEngine {
     public static String replaceTags(String message) {
         Pattern p = Pattern.compile("(#.*#)");
         Matcher m = p.matcher(message);
-        boolean result = m.find();
-        if (result) {
+        int i = 1;
+        if (m.find()) {
+            Optional<String> property =
+                    Optional.ofNullable(System.getProperty(m.group(i++).substring(1, m.group().length() - 1)));
+            if (property.isPresent()) {
+            } else {
+                throw new IllegalArgumentException();
+            }
             message = m.replaceAll("Hello");
         }
         return message;
